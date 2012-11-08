1fdba315910f322584139da3e738d4ce36c6d8b3
Ricardo Mendes <rokusu@gmail.com>
2012-11-08 11:27:46 +0000

    deleting stuff + small refactoring

--- js/github_repos.js
+++ js/github_repos.js
@@ -1,7 +1,7 @@
 var githubRepos = Sammy("#repos" , function () {
-
   this.use('Mustache','ms');
 
+  var endPoint = 'https://api.github.com/users/locks/repos?per_page=100&callback=?'
   var groupLanguages = function (repos) {
     return _(repos).chain()
                    .reject(function (r) { return r.fork })
@@ -11,25 +11,26 @@ var githubRepos = Sammy("#repos" , function () {
                    .value()
   }
 
-  this.get('/', function (ctx) {
-    $.getJSON('https://api.github.com/users/locks/repos?per_page=100&callback=?').success(function (data) {
+  var getJSON = function (ctx, callback) {
+    $.getJSON(endPoint).success(function (data) {
       data = groupLanguages(data['data'])
 
       ctx.langs = _(data).keys()
-      ctx.repos = _.compose(_.flatten, _.values)(data)
+      ctx.repos = callback(data);
 
-      ctx.partial('repositories.ms')
+      ctx.partial('templates/repositories.ms')
+    })
+  }
+
+  this.get('/', function (ctx) {
+    getJSON(ctx, function (data) {
+      return _.compose(_.flatten, _.values)(data)
     })
   })
 
-  this.get('#/language/:language' , function (ctx) {
-    $.getJSON('https://api.github.com/users/locks/repos?per_page=100&callback=?').success(function (data) {
-      data = groupLanguages(data['data'])
-
-      ctx.langs = _(data).keys()
-      ctx.repos = data[ctx.params.language]
-
-      ctx.partial('repositories.ms')
+  this.get('#/:language' , function (ctx) {
+    getJSON(ctx, function (data) {
+      return data[ctx.params.language]
     })
   })
 
